878e95a237c129d2694e8ac017848aba48eb1f2e
David Rocendo <rocendod@msu.edu>
2024-02-12 12:33:09 -0500

    Added custon css properties and shortcode improvements

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,17 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+  eleventyConfig.addShortcode("planetCard", function(planetName, au) {
+    return `<div class="card-container-2">
+<main class="card-content">
+    <figure class="${planetName}"></figure>
+        <li class="Name">
+          ${planetName}
+        </li>
+        <li>
+            AU: ${au}
+        </li>
+</main>
+</div>`
+  });
 };
